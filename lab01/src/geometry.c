51b7e105945e796096f5afe2aa9b1503b4b9e24a
Колоколов Глеб Игоревич <kgi23u917@student.bmstu.ru>
2025-02-20 12:04:06 +0300

    Use init function instead of bracket init

--- lab01/src/geometry.c
+++ lab01/src/geometry.c
@@ -12,26 +12,26 @@ vec3d_t create_vec3d(double x, double y, double z)
 
 point3d_t point_translate(const point3d_t *first, const point3d_t *second)
 {
-    return (point3d_t){first->x + second->x, first->y + second->y, first->z + second->z};
+    return create_vec3d(first->x + second->x, first->y + second->y, first->z + second->z);
 }
 
 point3d_t rotate_x(const point3d_t *point, double degree)
 {
-    return (point3d_t){point->x, point->y * cos(degree) + point->z * sin(degree), point->y * -sin(degree) + point->z * cos(degree)};
+    return create_vec3d(point->x, point->y * cos(degree) + point->z * sin(degree), point->y * -sin(degree) + point->z * cos(degree));
 }
 
 point3d_t rotate_y(const point3d_t *point, double degree)
 {
-    return (point3d_t){point->x * cos(degree) + point->z * -sin(degree), point->y, point->x * sin(degree) + point->z * cos(degree)};
+    return create_vec3d(point->x * cos(degree) + point->z * -sin(degree), point->y, point->x * sin(degree) + point->z * cos(degree));
 }
 
 point3d_t scale_point(const point3d_t *point, double scale)
 {
-    return (point3d_t){point->x * scale, point->y * scale, point->z * scale};
+    return create_vec3d(point->x * scale, point->y * scale, point->z * scale);
 }
 
 point3d_t projection(const point3d_t *point, double distance)
 {
     double z = 10 / (distance - point->z / 2);
-    return (point3d_t){point->x * z, point->y * z, point->z};
+    return create_vec3d(point->x / z, point->y / z, point->z);
 }
