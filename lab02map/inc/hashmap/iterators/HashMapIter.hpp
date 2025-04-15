61a0e952664766b078722675fb5d88461a7207e7
Колоколов Глеб Игоревич <kgi23u917@student.bmstu.ru>
2025-04-15 23:47:51 +0300

    Start working on hashmap iterator

--- lab02map/inc/hashmap/iterators/HashMapIter.hpp
+++ lab02map/inc/hashmap/iterators/HashMapIter.hpp
@@ -10,14 +10,20 @@ template <typename K, typename V>
 class HashMapIterator
 {
   public:
+	using size_type = std::size_t;
+	using value_type = HashMap<K, V>::value_type;
     using iterator = HashMapIterator<K, V>;
 
     HashMapIterator();
     HashMapIterator(const HashMap<K, V> &map);
     HashMapIterator(const HashMapIterator<K, V> &map);
-    HashMapIterator(const std::shared_ptr<Bucket<typename HashMap<K, V>::value_type>[]> &buckets);
+    HashMapIterator(const std::shared_ptr<Bucket<value_type>[]> &buckets);
 
   protected:
+	std::weak_ptr<Bucket<value_type>[]> bucketPtr;
+
+	size_type bucketIndex;
+	BucketIterator<value_type> localIterator;
 };
 
 #include <hashmap/iterators/HashMapIterImpl.hpp>
